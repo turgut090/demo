41f1dfe17f6778d8902fb187511c044f0b94a5ca
Turgut <turqut.a.314@gmail.com>
2021-03-06 20:52:26 +0400

    Update records.R

--- records.R
+++ records.R
@@ -11,7 +11,7 @@ library(plyr)
 library(doParallel)
 library(data.table)
 
-result11 <- data.frame(matrix(NA, nrow = 7, ncol = 1))
+result11 <- data.frame(matrix(NA, nrow = 8, ncol = 1))
 
 # get pages
 colnames(result11) <- c("reference")
@@ -112,7 +112,7 @@ for (i in 1:length(smp)) {
                html_nodes(".locations") %>%
                html_text() %>% str_split(.,'   ',simplify = T))
     return(res)
-  },mc.cores = 20)
+  },mc.cores = 10)
   list2[[i]]<-new
   print(i)
 }
