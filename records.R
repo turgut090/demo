f911da873812907408446b76842b57235457ffc8
Turgut <turqut.a.314@gmail.com>
2021-03-29 19:57:09 +0400

    Update records.R (#5)
    * Update records.R
    
    * Update records.R

--- records.R
+++ records.R
@@ -7,7 +7,7 @@ library(plyr)
 library(doParallel)
 library(data.table)
 
-result11 <- data.frame(matrix(NA, nrow = 8, ncol = 1))
+result11 <- data.frame(matrix(NA, nrow = 7, ncol = 1))
 
 # get pages
 colnames(result11) <- c("reference")
@@ -153,7 +153,7 @@ img_add_gather = list()
 for (i in 1:length(bina_links)) {
   imgs = read_html(bina_links[i]) %>% 
     html_nodes('.thumbnail') %>% html_attr('data-mfp-src')
-  idx = sample(1:length(imgs), floor(length(imgs)*0.9), replace=TRUE)
+  idx = sample(1:length(imgs), floor(length(imgs)*0.8), replace=TRUE)
   dir_name = paste('id_',str_extract(bina_links[i],'[0-9]+'),sep = '')
   dir.create(dir_name)
   for(j in idx) {
@@ -168,5 +168,4 @@ for (i in 1:length(bina_links)) {
   }
   print(paste('Done',i,'out of', length(bina_links)))
 }
-
 
